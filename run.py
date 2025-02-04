9210b641b3e13b92e182a45aca4182b72b2df991
tronic-man <143120468+tronic-man@users.noreply.github.com>
2025-02-04 13:20:41 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
-# This code is unfinished
-from selenium import webdriver
-import time
-import requests
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+import requests
+import time
 
-browser = webdriver.Firefox()
-browser.get("https://www.noip.com/login")
-browser.find_element(By.ID,"username").send_keys(email)
-with open("pass.txt", "r") as pw:
-    browser.find_element(By.ID,"password").send_keys(pw.readline().lstrip().rstrip())
-browser.find_element(By.ID,"clogs-captcha-button").click()
+with open("logins.txt", "r") as logindata:
+    Script_URL, email, password =  logindata.readlines()
+    
+    browser = webdriver.Firefox()
+    browser.get("https://www.noip.com/login")
+
+    # Enter No-IP Username and Password
+    browser.find_element(By.ID,"username").send_keys(email.strip())
+    browser.find_element(By.ID,"password").send_keys(password.strip())
+    browser.find_element(By.ID,"clogs-captcha-button").click()
 
 try:
+    # Fetch No-IP 2FA Code From Google Apps Script API
     time.sleep(10)
-    browser.find_element(By.ID, "firstInput").send_keys( requests.get(Script_URL).text.split('\\x26')[1] )
+    browser.find_element(By.ID, "firstInput").send_keys( requests.get(Script_URL.strip()).text.split('\\x26')[1] )
     browser.find_element(By.NAME, "submit").click()
 except:
     print('OTP Failed')
